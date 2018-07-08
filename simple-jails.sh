e2702dcfb494f9a1ea85221fa528db74f683c751
foudfou <foudil.newbie+git@gmail.com>
2018-07-08 17:30:38 +0200

    Various fixes, add usage and basic option parsing

--- simple-jails.sh
+++ simple-jails.sh
@@ -32,7 +32,7 @@ EOF
 sjail_fetch(){
     version="$1"
     echo "Setting up base for ${version}"
-    source /etc/simple-jails.conf
+    . /etc/simple-jails.conf
 
     if [ -e ${zfs_jail_mount}/templates/base-${version} ]; then
         echo "Error: $version already fetched" 1>&2
@@ -55,21 +55,22 @@ sjail_fetch(){
     done
 
     for i in $to_copy; do
-        cp ${i} "${zfs_jail_mount}/templates/base-${version}/etc/${i}"
+        cp ${i} "${zfs_jail_mount}/templates/base-${version}${i}"
     done
 }
 
 sjail_update(){
     version="$1"
     echo "Updating ${version}"
-    source /etc/simple-jails.conf
+    . /etc/simple-jails.conf
     env UNAME_r=${version} freebsd-update -b ${zfs_jail_mount}/templates/base-${version} fetch install
+    env UNAME_r=${version} freebsd-update -b ${zfs_jail_mount}/templates/base-${version} IDS
 }
 
 sjail_set_skel(){
     version="$1"
     echo "Setting up skeleton for ${version}"
-    source /etc/simple-jails.conf
+    . /etc/simple-jails.conf
     if [ -e ${zfs_jail_mount}/templates/skeleton-${version} ]; then
         echo "Warning: skeleton for $version already created" 1>&2
         return 0
@@ -81,10 +82,12 @@ sjail_set_skel(){
         mkdir -p "${zfs_jail_mount}/templates/skeleton-${version}/${i}"
     done
 
+    chflags noschg "${zfs_jail_mount}/templates/base-${version}/var/empty"
     to_move_dirs="etc usr/local tmp var root"
     for i in $to_move_dirs; do
         mv "${zfs_jail_mount}/templates/base-${version}/${i}" "${zfs_jail_mount}/templates/skeleton-${version}/${i}"
     done
+    chflags schg "${zfs_jail_mount}/templates/skeleton-${version}/var/empty"
 
     current_dir=$(pwd)
     cd ${zfs_jail_mount}/templates/base-${version}
@@ -108,7 +111,7 @@ sjail_create_thinjail() {
     version="$1"
     jail_name="$2"
     echo "Setting up jail: $jail_name"
-    source /etc/simple-jails.conf
+    . /etc/simple-jails.conf
     if [ ! -e ${zfs_jail_mount}/thinjails ]; then
         zfs create ${zfs_data_set}/thinjails
     fi
@@ -119,11 +122,51 @@ sjail_create_thinjail() {
     fi
 
     zfs clone ${zfs_data_set}/templates/skeleton-${version}@skeleton ${zfs_data_set}/thinjails/${jail_name}
-    echo hostname=\"${jail_name}\" > ${zfs_jail_mount}thinjails/${jail_name}/etc/rc.conf
+    echo hostname=\"${jail_name}\" > ${zfs_jail_mount}/thinjails/${jail_name}/etc/rc.conf
 
     mkdir -p ${zfs_jail_mount}/${jail_name}
     cat <<EOF > ${zfs_jail_mount}/${jail_name}.fstab
-${zfs_jail_mount}/templates/${version}-RELEASE  ${zfs_jail_mount}/${jail_name}/ nullfs   ro          0 0
-${zfs_jail_mount}/thinjails/${jail_name}     ${zfs_jail_mount}/${jail_name}/skeleton nullfs  rw  0 0
+${zfs_jail_mount}/templates/base-${version}	${zfs_jail_mount}/${jail_name}/ nullfs ro 0 0
+${zfs_jail_mount}/thinjails/${jail_name}	${zfs_jail_mount}/${jail_name}/skeleton nullfs rw 0 0
 EOF
 }
+
+
+usage() {
+    cat <<EOF
+Usage: `basename $0` COMMAND [args]
+
+Commands:
+    init                 Install initial config file (/etc/simple-jails.conf)
+    fetch VERSION        Fetch and create base template for version VERSION
+                         (e.g. 11.2-RELEASE)
+    update VERSION       Apply freebsd-update on base template with version
+                         VERSION. Can be done anytime.
+    skel VERSION         Create skeleton from base template version VERSION
+    create VERSION NAME  Create thin jail NAME from base template with version
+                         VERSION. Update /etc/jail.conf manually.
+EOF
+    exit 0
+}
+
+command=$1; shift
+case $command in
+    "init")
+        sjail_init
+        ;;
+    "fetch")
+        sjail_fetch "$@"
+        ;;
+    "update")
+        sjail_update "$@"
+        ;;
+    "skel"*)
+        sjail_set_skel "$@"
+        ;;
+    "create")
+        sjail_create_thinjail "$@"
+        ;;
+    *)
+        usage
+        ;;
+esac
